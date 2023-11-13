208b8fb37bca0a0ee718f3910c85d6f2a4251cf1
Marc Roman Colom <marcromancolom@gmail.com>
2023-11-13 16:33:58 +0100

    added condition to check if role exists

--- main.php
+++ main.php
@@ -27,29 +27,33 @@ include_once("phpQs/config.php");
         <div>Here there's gonna be the search bar</div>
         <div>static basket or cesta for the buyer</div>
         <?php
-
+        if(!empty($role))
+        {
         switch ($role) {
             case 'vendor':
                 echo "<div>Dynamic added button or smt for the vendor</div>";
                 break;
             case 'controler':
-                echo "<div>Dynamic added button or smt for the vendor</div>";
+                echo "<div>Dynamic added button or smt for the controler</div>";
                 break;
-            default:
-                # code...
-                break;
+        }
         }
         echo "<div>Config reference for the user</div>";
         ?>
 
-        <div>Dynamic added button or smt for the controler</div>
+        
     </div>
     This is static bloody text, just as Marcazon theme.
     <div class="category-filter">
 
     </div>
     <div class="product-list">
-        <div class="product"> this is a product</div>
+        <?php
+        $products = mysqli_query($conn,"SELECT * FROM Producte");
+        while ($product = mysqli_fetch_array($products)) {
+            echo "<div class=\"product\">".$product["nomProd"]."</div>";
+        }
+        ?>
         <div class="product"> this is a product</div>
         <div class="product"> this is a product</div>
     </div>
