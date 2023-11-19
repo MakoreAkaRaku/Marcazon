c9e31d7a74f64760e6d1119a44ef8ab76b9a2181
MakoreAkaRaku <marcromancolom@gmail.com>
2023-11-19 02:36:35 +0100

    Added viewport so others devices can use the browser on it as well

--- config/head.php
+++ config/head.php
@@ -4,5 +4,6 @@ echo "<head>
     <link rel=\"stylesheet\" href=\"pending\">
     <link rel=\"icon\" href=\"./styles/ico.ico\">
     <script src=\"https://cdn.tailwindcss.com\"></script>
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 </head>";
 ?>
