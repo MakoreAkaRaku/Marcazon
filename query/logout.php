d4551b1061610420ee37e31723767c9ba9032b5e
Marc Roman Colom <marcromancolom@gmail.com>
2023-11-20 14:38:57 +0100

    Created logout functionality

---
+++ query/logout.php
@@ -0,0 +1,11 @@
+<?php
+include_once("../logic/logInOut.php");
+if (!empty($_SESSION)) {
+    //If we did not sign out correctly, handle the error.
+    if (!logout()) {
+        die("Something is of... did you login correctly..?");
+    }
+}
+header('location: /Marcazon/main.php');
+exit;
+?>
