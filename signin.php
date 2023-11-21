770b898281f324b4eb75deae25c2101efddb90cb
MakoreAkaRaku <marcromancolom@gmail.com>
2023-11-21 18:37:19 +0100

    updated head implementation,added ''Go back''

--- signin.php
+++ signin.php
@@ -1,24 +1,25 @@
 <?php
+include_once("components/reusable.php");
 include_once("config/config.php");
 if (!empty($_SESSION)) {
   header('location: /Marcazon/main.php');
   exit;
 }
-include_once("components/elements.php");
 
 $products = mysqli_query($GLOBALS["conn"], "SELECT * FROM Producte");
 ?>
 <!DOCTYPE html>
 <html>
-<?php
-include_once("config/head.php");
-?>
+<?php head(); ?>
 
 <body class="h-screen overflow-hidden flex items-center justify-center"
   style="background-image: linear-gradient(45deg, #FC466B 0%, #3F5EFB 100%)">
   <div
     class="flex flex-col p-6 bg-black/70 w-full m-6 md:m-0 md:w-2/3 flex items-center justify-center rounded-lg max-w-md">
-    <?php h1("INICIAR SESSIÓ", "text-white text-center") ?>
+    <?php
+    a("./main.php", "➥ Inici", "flex px-2 py-2 bg-black text-xl text-white text-center self-start rounded-lg hover:bg-gray-900");
+    h1("INICIAR SESSIÓ", "text-white text-center")
+      ?>
     <form action="query/login.php" method="POST" class="flex flex-col gap-4 w-full ">
       <?php
       $inClasses = 'focus:border-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300';
@@ -31,7 +32,8 @@ include_once("config/head.php");
       </div>
     </form>
     <div class="mt-4 text-sm text-white text-center">
-      <p>No tens compte? <a href="./signup.php" class="text-snow hover:underline">Registrar-se</a>
+      <p>No tens compte?
+        <?php a("./signup.php", "Registrar-se", "text-snow hover:underline"); ?>
       </p>
     </div>
   </div>
