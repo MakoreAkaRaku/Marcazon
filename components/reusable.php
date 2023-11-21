88ccf7dbfb14da06411301f7b1f353ca27884dbe
MakoreAkaRaku <marcromancolom@gmail.com>
2023-11-21 21:31:46 +0100

    Added all content into reusable for MainNavBar

--- components/reusable.php
+++ components/reusable.php
@@ -18,9 +18,80 @@ function displayProducts()
     echo '</div>';
     h1("PRODUCTES", "justify-center text-lg text-white");
     echo '<div class="product-list">';
-        while ($product = mysqli_fetch_array($Allproducts)) {
-            echo "<div class=\"product\">" . $product["nomProd"] . "</div>";
-        }
+    while ($product = mysqli_fetch_array($Allproducts)) {
+        echo "<div class=\"product\">" . $product["nomProd"] . "</div>";
+    }
     echo '</div>';
+}
+
+function logged_in_gui($nick)
+{
+    echo "<div class=\"user-icon\">UserIconIdentity</div>";
+    $time = date("G", time());
+    if ($time >= 20) {
+        $welcomeUser = "Bona nit";
+    } elseif ($time >= 13) {
+        $welcomeUser = "Bona tarda";
+    } else {
+        $welcomeUser = "Bon dia";
+    }
+    echo $welcomeUser . ", " . $nick;
+    a('query/logout.php', 'Tanca Sessió', ""); //FIXME
+}
+function unlogged_gui()
+{
+    echo "<div class=\"user-icon\">UserIconNonIdentity</div>";
+    echo '<div class="flex flex-col text-center">';
+    a('signin.php', "Inicia Sessió", "");
+    p("o bé", "text-sm");
+    a('signup.php', "Registra't", "");
+    echo '</div>';
+}
+
+/**
+ * It returns a gradient background look depending on user role.
+ */
+function custom_bg()
+{
+    $role = has_session() ? $_SESSION['role'] : '';
+    $customClasses = 'bg-gradient-to-b bg-no-repeat bg-full bg-fixed ';
+    switch ($role) {
+        case 'Venedor':
+            $customClasses .= "from-red-500";
+            break;
+        case 'Comprador':
+            $customClasses .= "from-green-500";
+            break;
+        case 'Controlador':
+            $customClasses .= "from-orange-400";
+            break;
+        default:
+            $customClasses .= "from-indigo-400";
+    }
+    $customClasses .= " to-black";
+    echo $customClasses;
+}
+
+function applyMainNavBar()
+{
+    
+    $mainClasses = "flex flex-row bg-teal-400 justify-between shadow-xl px-4";
+    echo '<nav role="navigation" ';
+    applyClasses($mainClasses, '');
+    echo '>';
+    echo '<div class="flex flex-row inline-flex">';
+    has_session() ? logged_in_gui($_SESSION['user']) : unlogged_gui();
+    echo '</div>';
+    echo "<div>Here there's gonna be the search bar</div>";
+    echo "<div>static basket or cesta for the buyer</div>";
+    if (has_session()) {
+        $role = $_SESSION['role'];
+        $srcImg = 'styles/';
+        $srcImg .= ($role == 'Venedor') ? "price-tag.svg" : "control-opt.svg";
+        echo "<div>";
+        img($role . ' options', $srcImg, "h-10");
+        echo "</div>";
+    }
+    echo '</nav>';
 }
 ?>
--- main.php
+++ main.php
@@ -1,58 +1,16 @@
 <?php
-include_once("components/elements.php");
+include_once("components/reusable.php");
 include_once("config/config.php");
-$Allproducts = mysqli_query($GLOBALS["conn"], "SELECT * FROM Producte");
 ?>
 <!DOCTYPE html>
 <html>
-<?php
-include_once("config/head.php");
-?>
+<?php head(); ?>
 
-<body style="background-image: url('styles/pet-steve.jpg'); background-repeat: repeat;">
-    <div class="nav-bar flex flex-row justify-around bg-teal-400">
-        <?php
-        $userIconStyle= '';
-        if (!empty($_SESSION)) {
-            $nick = $_SESSION['user'];
-            $role = $_SESSION['role'];
-            echo "<div class=\"user-icon\">UserIconIdentity</div>";
-            echo "Bones, $nick";
-            a('query/logout.php','Tanca Sessió',""); //TMP
-        } else {
-            echo "<div class=\"user-icon\">UserIconNonIdentity</div>";
-            echo '<div class="flex flex-col text-center">';
-            a('signin.php',"Inicia Sessió","");
-            p("o bé","text-sm");
-            a('signup.php',"Registra't","");
-            echo '</div>';
-        }
-        ?>
-        <div>Here there's gonna be the search bar</div>
-        <div>static basket or cesta for the buyer</div>
-        <?php
-        if (!empty($role))
-        {
-            $srcImg = ($role == "vendor") ? "price-tag.svg":"control-opt.svg";
-            echo "<div> <img alt=\"$role options\" src=$srcImg></div>";
-        }
-         if (!empty ($nick))
-         {
-            echo "<div> <img alt=\"user options\" src=styles/config.svg></div>";
-         }
-        ?>
-    </div>
-    This is static bloody text, just as Marcazon theme.
-    <div class="category-filter">
-
-    </div>
-    <div class="product-list">
-        <?php
-        while ($product = mysqli_fetch_array($Allproducts)) {
-            echo "<div class=\"product\">" . $product["nomProd"] . "</div>";
-        }
-        ?>
-    </div>
+<body class="<?php custom_bg() ?>">
+    <?php
+    applyMainNavBar();
+    displayProducts();
+    ?>
 </body>
 
 </html>
