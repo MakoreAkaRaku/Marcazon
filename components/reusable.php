821e264e9bc76643c82d45522c7b502bc7fdeab1
Marc Roman Colom <marcromancolom@gmail.com>
2023-11-23 14:19:32 +0100

    Removed useless classes and upgraded background

--- components/reusable.php
+++ components/reusable.php
@@ -51,7 +51,7 @@ function unlogged_gui()
 function addNavSearchBar()
 {
     echo '<form class="relative w-full">';
-    input('search','product-search','p-4 w-full text-sm text-gray-900 bg-gray-700 border-none dark:bg-gray-700  dark:placeholder-gray-400 dark:text-white',"Cerca productes...");
+    input('search','product-search','p-4 placeholder-gray-400 text-sm text-white bg-cyan-800 border-none dark:text-white',"Cerca productes...");
     echo '<button class="absolute end-0 p-4 bg-lime-950 rounded-md" type="submit">';
     /*echo '<svg class="" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
